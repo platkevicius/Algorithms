d8aae682b9944427c7d4769945a7e1047c4efba8
Daniel <dethrimlp@gmail.com>
2021-11-08 22:23:08 +0100

    added memoization

--- src/dp/topdown/RemovingDigits.java
+++ src/dp/topdown/RemovingDigits.java
@@ -1,14 +1,19 @@
 package dp.topdown;
 
+import java.util.Arrays;
+
 public class RemovingDigits {
 
     public static void main(String[] args) {
         RemovingDigits removingDigits = new RemovingDigits();
-        System.out.println(removingDigits.solve(27));
+        int[] dp = new int[28];
+        Arrays.fill(dp, -1);
+        System.out.println(removingDigits.solve(27, dp));
     }
 
-    public int solve(int n) {
+    public int solve(int n, int dp[]) {
         if (n == 0) return 0;
+        if (dp[n] != -1) return dp[n];
 
         int ans = Integer.MAX_VALUE;
 
@@ -16,10 +21,11 @@ public class RemovingDigits {
         while(subDigits != 0) {
             int value = subDigits % 10;
             if (n - value >= 0 && value != 0)
-                ans = Math.min(ans, 1 + solve(n - value));
+                ans = Math.min(ans, 1 + solve(n - value, dp));
 
             subDigits /= 10;
         }
+        dp[n] = ans;
         return ans;
     }
 
